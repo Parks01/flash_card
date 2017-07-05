b172b6333c1857844231c359d3d1c672e922da69
Andrew Parks and Lois Choi <andrew@>
2017-07-05 09:58:24 -0700

    add definitons to vocab words, add CSS style to h1, buttons, and h3

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,26 @@
 $(document).ready(function() {
   $(".javacript-shown").click(function() {
     $(".javascript-hidden").toggle();
-  })
+  });
   $(".operator-shown").click(function() {
     $(".operator-hidden").toggle();
-  })
+  });
+  $(".naming-shown").click(function() {
+    $(".naming-hidden").toggle();
+  });
+  $(".function-shown").click(function() {
+    $(".function-hidden").toggle();
+  });
+  $(".methods-shown").click(function() {
+    $(".methods-hidden").toggle();
+  });
+  $(".arguments-shown").click(function() {
+    $(".arguments-hidden").toggle();
+  });
+  $(".parameters-shown").click(function() {
+    $(".parameters-hidden").toggle();
+  });
+  $(".variable-shown").click(function() {
+    $(".variable-hidden").toggle();
+  });
 });
