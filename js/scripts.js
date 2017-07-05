a1ff091ac1058cf0343ab0d0f7fffa22a7e4d256
Andrew Parks and Lois Choi <andrew@>
2017-07-05 09:27:33 -0700

    change css and js to html

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $(".javacript-shown").click(function() {
+    $(".javascript-hidden").toggle();
+  })
+  $(".operator-shown").click(function() {
+    $(".operator-hidden").toggle();
+  })
+});
